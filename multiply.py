b3ddff6608fe0040f629b80921cecced9b40cbed
Joris van Rooij <jorisv@chalmers.se>
2019-05-29 10:37:43 +0200

    Success!!

--- multiply.py
+++ multiply.py
@@ -4,6 +4,7 @@ from pyspark import SparkContext
 from pyspark.sql import SparkSession
 from pyspark.mllib.linalg import Matrices
 from pyspark.mllib.linalg.distributed import BlockMatrix
+from pyspark.mllib.linalg.distributed import IndexedRowMatrix
 # from pyspark.mllib.linalg.distributed import *
 import numpy
 from scipy.io import mmread
@@ -12,28 +13,20 @@ from scipy.sparse import coo_matrix
 appName = "MultiplyApp"
 local = "local"
 local_parallel = "local[*]"
+nRowsPerBlock = 5 # Need to set this to something useful
+nColumnsPerBlock = 5 # Need to set this to something useful
 conf = SparkConf().setAppName(appName).setMaster(local)
 sc = SparkContext(conf=conf)
 spark = SparkSession(sc)
 
-mat = mmread("data/rel3.mtx").tocsc()
-numRows = mat.shape[0]
-numCols = mat.shape[1]
-smx = Matrices.sparse(numRows, numCols, mat.indptr, mat.indices, mat.data)
-print(smx)
+mat = mmread("data/rel3.mtx")
 
-dm1 = Matrices.dense(3, 2, [1, 2, 3, 4, 5, 6])
-dm2 = Matrices.dense(3, 2, [7, 8, 9, 10, 11, 12])
-sm = Matrices.sparse(3, 2, [0, 1, 3], [0, 1, 2], [7, 11, 12])
-blocks1 = sc.parallelize([((0, 0), dm1), ((1, 0), dm2)])
-blocks2 = sc.parallelize([((0, 0), dm1), ((1, 0), dm2)])
-blocks3 = sc.parallelize([((0, 0), sm), ((1, 0), dm2)])
-mat1 = BlockMatrix(blocks1, 3, 2)
-mat2 = BlockMatrix(blocks2, 3, 2)
-mat3 = BlockMatrix(blocks3, 3, 2)
+indexed_rows = sc.parallelize(enumerate(mat.toarray()))
+indexed_row_matrix = IndexedRowMatrix(indexed_rows)
+block_matrix = indexed_row_matrix.toBlockMatrix(nRowsPerBlock,nColumnsPerBlock)
 
-mat1.add(mat2).toLocalMatrix()
-mat1.add(mat3).toLocalMatrix()
+print(block_matrix.add(block_matrix).toLocalMatrix())
+
 
 print("done")
 
