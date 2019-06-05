e64db77cda7e45b4375c211ddace5379f4605091
Joris van Rooij <jorisv@chalmers.se>
2019-06-05 10:22:56 +0200

    Speedup graphs and text

--- filter.py
+++ filter.py
@@ -1,5 +1,6 @@
 import re
 import matplotlib.pyplot as plt
+import numpy as np
 
 def num_nodes(line):
     return re.search('Resizing cluster to (\d) nodes', line)
@@ -24,22 +25,39 @@ def process(line):
 num_nodes_list = []
 parallelize_duration_list = []
 calculation_duration_list = []
-file = 'data/multiply_filtered.log'
+file = 'data/svd_filtered.log'
 with open(file, 'r') as f:
+    current_num_node = 0
+    parallellize_list = []
+    calculation_list = []
     for line in f:
         if num_nodes(line):
             n = int(num_nodes(line).group(1))
             num_nodes_list.append(n)
+            if n != current_num_node and n > 2:
+                current_num_node = n
+                parallelize_duration_list.append(np.mean(parallellize_list))
+                parallellize_list = []
+                calculation_duration_list.append(np.mean(calculation_list))
+                calculation_list = []
         elif parallelize_duration(line):
             n = float(parallelize_duration(line).group(1))
-            parallelize_duration_list.append(n)
+            parallellize_list.append(n)
         elif calculation_duration(line):
             n = float(calculation_duration(line).group(1))
-            calculation_duration_list.append(n)
+            calculation_list.append(n)
         else:
             print('Unparsable log format in line:' + line)
-
+    parallelize_duration_list.append(np.mean(parallellize_list))
+    calculation_duration_list.append(np.mean(calculation_list))
+    
 # Plot line graph with the two series parallelize and calculation (including labels)
-plt.plot(num_nodes_list, parallelize_duration_list, 'r')
-plt.plot(num_nodes_list, calculation_duration_list, 'b')
-plt.show()
+fig,ax = plt.subplots()
+ax.plot(num_nodes_list, parallelize_duration_list, 'r', label='parallelisation')
+ax.plot(num_nodes_list, calculation_duration_list, 'b', label='calculation')
+ax.plot(num_nodes_list, [a + b for a, b in zip(parallelize_duration_list, calculation_duration_list)], 'g', label='total')
+ax.set(xlabel='Number of worker nodes', ylabel='Execution time (s)',
+       title='SVD calculation')
+ax.legend()
+#plt.xticks(num_nodes_list)
+plt.savefig("report/img/svd-speedup.pdf")
